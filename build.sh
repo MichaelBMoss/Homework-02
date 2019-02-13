c2394a82d9f42202186db3d4de1357b1faf5df70
Mike Moss' Ubuntu Linux <michaelbmoss@gmail.com>
2019-02-13 15:51:33 -0800

    completed bash build file

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
 cat templates/top.html content/home.html templates/bottom.html > docs/mike_moss_home.html
+
+cat templates/top.html content/bio.html templates/bottom.html > docs/mike_moss_bio.html
+
+cat templates/top.html content/contact.html templates/bottom.html > docs/mike_moss_contact.html
